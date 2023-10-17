c696f1a94736b015446178181f3c260f5ac539b9
yongsin0129Laptop <yongsin@gmail.com>
2023-10-17 11:54:13 +0800

    Filtering tasks

--- app.js
+++ app.js
@@ -83,10 +83,26 @@ class Root extends Component {
         <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-todo" type="text" />
         <div class="task-list">
           <t t-foreach="store.tasks" t-as="task" t-key="task.id">
-            <Task task="task" /> 
+            <Task task="task" />
           </t>
         </div>
-    </div>
+        <div class="task-panel" t-if="store.tasks.length">
+          <div class="task-counter">
+            <t t-esc="displayedTasks.length"/>
+            <t t-if="displayedTasks.length lt store.tasks.length">
+                / <t t-esc="store.tasks.length"/>
+            </t>
+            task(s)
+          </div>
+          <div>
+            <span t-foreach="['all', 'active', 'completed']"
+              t-as="f" t-key="f"
+              t-att-class="{active: filter.value===f}"
+              t-on-click="() => this.setFilter(f)"
+              t-esc="f"/>
+          </div>
+        </div>
+      </div>
 `;
 
 
@@ -96,6 +112,7 @@ class Root extends Component {
     const useref = useRef("add-todo")
     onMounted(() => useref.el.focus())
     this.store = useStore() // return useState(useEnv().store)
+    this.filter = useState({ value: "all" })
   }
 
   addTask (ev) {
@@ -105,7 +122,18 @@ class Root extends Component {
     }
   }
 
+  get displayedTasks () {
+    const tasks = this.store.tasks
+    switch (this.filter.value) {
+      case "active": return tasks.filter(t => !t.isCompleted)
+      case "completed": return tasks.filter(t => t.isCompleted)
+      case "all": return tasks
+    }
+  }
 
+  setFilter (filter) {
+    this.filter.value = filter
+  }
 }
 
 // --------------------------------------------------------
